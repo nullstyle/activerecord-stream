2faae917d94931bebd80ee9b8b1fbd03a6c66601
Scott Fleckenstein <nullstyle@gmail.com>
2013-03-25 09:59:26 -0700

    Renames filter => select, and add reject support as well

--- lib/core_ext/enumerator/filter.rb
+++ lib/core_ext/enumerator/filter.rb
@@ -1,18 +1,27 @@
 module Enumerator::Filter
-  def filter(&block)
-    
+  def select(&block)
+    filter(true, &block)
+  end
+
+  def reject(&block)
+    filter(false, &block)
+  end
+
+  private
+  def filter(filter_val, &block)
     Enumerator.new do |yielder|
 
       begin
         loop do
           val = self.next
-          should_yield = block.call(val)
+          should_yield = block.call(val) == filter_val
           yielder << val if should_yield
         end
       rescue StopIteration
       end
     end
   end
+
 end
 
 Enumerator.send(:include, Enumerator::Filter)
